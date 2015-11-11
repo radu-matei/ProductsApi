b0352a5dff8756965eadd3e8bed0ad43330d4f37
radu-matei <radu.matei@equilobe.com>
2015-11-11 13:52:37 +0200

    Added ProductsController

---
+++ ProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using ProductsApi.DAL;
+using ProductsApi.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ProductsApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private static ProductsRepository productsRepository = new ProductsRepository();
+
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            return productsRepository.GetProducts();
+        }
+
+        [HttpPost]
+        public void Create(Product product)
+        {
+            productsRepository.Add(product);
+        }
+
+        [HttpGet]
+        public Product GetById(int id)
+        {
+            return productsRepository.GetById(id);
+        }
+    }
+}
