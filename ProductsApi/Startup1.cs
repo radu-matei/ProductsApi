e906d6d23b412a0f6c8ca6086fa722c74c4ea2e9
radu-matei <radu.matei@equilobe.com>
2015-12-03 21:03:25 +0200

    Added Startup class

---
+++ ProductsApi/Startup1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(ProductsApi.Startup1))]
+
+namespace ProductsApi
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR(new HubConfiguration()
+            {
+                EnableDetailedErrors = true
+            });
+        }
+    }
+}
